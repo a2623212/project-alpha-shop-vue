9451c941838a97672b3480728d8873db78ee73d4
a2623212@gmail.com <A5GjZK7w>
2022-02-11 17:06:21 +0800

    feat: import the components

--- src/views/CheckOut.vue
+++ src/views/CheckOut.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>this is Checkout.vue</h1>
+  <div class="container">
+    <h1>結帳</h1>
+    <div class="stepper">
+      <Stepper />
+    </div>
+    <div>
+      <form>
+        <router-view />
+      </form>
+    </div>
+    <div>
+      <ShoppingCart />
+    </div>
+  </div>
 </template>
+
+<script>
+import Stepper from "./../components/Stepper";
+import ShoppingCart from "./../components/ShoppingCart";
+export default {
+  name: "CheckOut",
+  components: {
+    Stepper,
+    ShoppingCart,
+  },
+};
+</script>
