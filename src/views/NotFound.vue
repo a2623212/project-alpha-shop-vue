499a37216b3978f6f3f306ad6200dbd10c3d41a7
a2623212@gmail.com <A5GjZK7w>
2022-02-11 12:45:29 +0800

    feat: Initial project
    clear the default file created by Vue CLI

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>404 Page Not Found</h1>
+    <img src="../assets/404.png" alt="404" class="img" />
+  </div>
+</template>
+
+<style scoped>
+.img {
+  margin: 0 auto;
+  width: 500px;
+  border-radius: 20px;
+}
+</style>
