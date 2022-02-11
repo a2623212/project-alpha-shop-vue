5a358233a62686d5bb9b2b053a4880c05e01395a
a2623212@gmail.com <A5GjZK7w>
2022-02-11 18:57:19 +0800

    feat: add the style of not found page
    make sure the scss settings could work

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,18 +1,6 @@
 <template>
-  <div>
+  <div class="not-found container">
     <h1>404 Page Not Found</h1>
     <img src="../assets/404.png" alt="404" class="img" />
   </div>
 </template>
-
-<style scoped lang="scss">
-@import "./../styles/color.scss";
-h1 {
-  color: $mainColor;
-}
-.img {
-  margin: 0 auto;
-  width: 500px;
-  border-radius: 20px;
-}
-</style>
