077f950fab2011b6142110ec869ab7251f94ef1e
a2623212@gmail.com <A5GjZK7w>
2022-02-11 18:08:39 +0800

    feat: add the template

--- src/views/CheckOut.vue
+++ src/views/CheckOut.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="container">
-    <h1>結帳</h1>
-    <div class="stepper">
-      <Stepper />
+  <main class="container">
+    <h2 id="main-title">結帳</h2>
+    <div id="main-content">
+      <section class="stepper">
+        <Stepper />
+      </section>
+      <section class="form">
+        <div id="main-content__form">
+          <div id="main-content__form__form-panel">
+            <form></form>
+          </div>
+        </div>
+      </section>
+      <section class="shopping-cart">
+        <Shopping-cart />
+      </section>
+
+      <div id="btn-control">
+        <button
+          id="btn-previous"
+          class="btn-previous arrow-previous btn-d-none"
+        >
+          上一步
+        </button>
+        <button id="btn-next" class="btn-next arrow-next">下一步</button>
+        <button id="btn-done" class="btn-done btn-d-none">確認下單</button>
+      </div>
     </div>
-    <div>
-      <form>
-        <router-view />
-      </form>
-    </div>
-    <div>
-      <ShoppingCart />
-    </div>
-  </div>
+  </main>
 </template>
 
 <script>
